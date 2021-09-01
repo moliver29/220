a1b5bf523e33fefccf192f646d50d2e1e59147dc
maxoliver <mjoliver529@gmail.com>
2021-09-01 13:05:06 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,13 @@
+"""
+Name: Max Oliver
+hello.py
+
+Problem: this program prints the statement hello, world!
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work
+"""
+def main():
+    print("hello, world!")
+
+main()
