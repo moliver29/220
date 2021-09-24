cda7be4d7e284e6aba9bb924bedda01bd776c62c
maxoliver <mjoliver529@gmail.com>
2021-09-23 23:21:03 -0400

    completed hw3

---
+++ assignments/hw3/mean.py
@@ -0,0 +1,19 @@
+"""
+Name: Max Oliver
+mean.py
+
+Problem: calculate the RMS, harmonica mean, and geometric mean of a set of values.
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+
+def main():
+    values = eval(input("Enter the number of values to be entered:"))
+    for i in range(values):
+        eval(input("enter value:"))
+
+
+if __name__ == '__main__':
+    main()
