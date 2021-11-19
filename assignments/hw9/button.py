1ade0504c96c0fa5fcb73531999c92c5ede72426
maxoliver <mjoliver529@gmail.com>
2021-11-18 22:57:24 -0500

    completed HW9

---
+++ assignments/hw9/button.py
@@ -0,0 +1,17 @@
+class Button:
+    def __init__(self, win, center, width, height, label):
+        door = Button(win, centerPoint, width, height, 'door')
+        w, h =
+
+    def get_label(self):
+        return self.label
+
+    def draw(win):
+
+    def undraw(self):
+
+    def is_clicked(point):
+
+    def color_button(color):
+
+    def set_label(label):
---
+++ assignments/hw9/threeDoorGame.py
@@ -0,0 +1,19 @@
+"""
+Name: Max Oliver
+threeDoorGame.py
+
+Problem: create a display that has a three door game
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+from graphics import *
+from Button import *
+
+
+def main():
+    win = GraphWin("Three Door Game", 500, 500)
+
+
+if __name__ == '__main__':
+    main()
